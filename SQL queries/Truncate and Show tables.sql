54a12ab03df7f4c8499abdd98c9f0a45d7070613
Adel  Hany Abdelfatah <99558161+adelhany1@users.noreply.github.com>
2023-06-06 22:34:09 +0300

    Add files via upload

---
+++ SQL queries/Truncate and Show tables.sql
@@ -0,0 +1,10 @@
+truncate table fact_transaction
+truncate table dim_audit
+truncate table err_destination_output
+truncate table err_source_output
+
+
+Select * from fact_transaction;
+Select * from dim_audit;
+Select * from err_destination_output
+Select * from err_source_output
